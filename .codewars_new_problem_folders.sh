87f50e433ea3a0453fc9322d3255092e1b8b8f27
Raul Penaguiao <raul.penaguiao@math.uzh.ch>
2022-04-02 17:23:03 -0700

    moved everything to folder javascript

--- .codewars_new_problem_folders.sh
+++ .codewars_new_problem_folders.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+cd javascript
 ls
+cd ..
 echo -n "please input folder name: "
 read foldername
 echo "your foldername is "
 echo $foldername
 cp -r ../../template/ ../codewars/
-mv template/ $foldername
-vim $foldername/assets/js/main.js
+mv template/ javascript/$foldername
+vim javascript/$foldername/assets/js/main.js
 echo "now add new files to the git"
--- .codewars_push_new_problem.sh
+++ .codewars_push_new_problem.sh
@@ -3,7 +3,7 @@ read foldername
 echo "Which git message do you want to leave: "
 read message
 x="/assets/js/main.js"
-git add "$foldername"/assets/js/main.js
+git add javascript/"$foldername"/assets/js/main.js
 git commit -m "bash message:  $message "
 ssh-add ~/.ssh/id_ed25519
 git push
