8a0c1e70d3675db4ca58955f301db9e0329dc426
Raul Penaguiao <raul.penaguiao@math.uzh.ch>
2022-04-03 11:57:48 -0700

    bash message:  solving arithmetics with functions

---
+++ python/002arithmetics_with_functions/002arithmetics_with_functions.py
@@ -0,0 +1,130 @@
+def zero(input = []):
+    #your code here
+    if input == []:
+        return 0
+    if input[0] == "x":
+        return 0 * input[1]
+    if input[0] == "+":
+        return 0 + input[1]
+    if input[0] == "-":
+        return 0 - input[1]
+    if input[0] == "/":
+        return 0 / input[1]
+def one(input = []):
+    x = 1
+    #your code here
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def two(input = []): #your code here
+    x = 2
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def three(input = []): #your code here
+    x = 3
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def four(input = []): #your code here
+    x = 4
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def five(input = []): #your code here
+    x = 5
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def six(input = []): #your code here
+    x = 6
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def seven(input = []): #your code here
+    x = 7
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def eight(input = []): #your code here
+    x = 8
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+def nine(input = []): #your code here
+    x = 9
+    if input == []:
+        return x
+    if input[0] == "x":
+        return x * input[1]
+    if input[0] == "+":
+        return x + input[1]
+    if input[0] == "-":
+        return x - input[1]
+    if input[0] == "/":
+        return x / input[1]
+
+def plus(input): #your code here
+    return ["+", input]
+def minus(input): #your code here
+    return ["-", input]
+def times(input): #your code here
+    return ["x", input]
+def divided_by(input): #your code here
+    return ["/", input]
